0647edb6f61ea771c2e3ca09c2890a4e2e11cbed
maxu6868 <493957049@qq.com>
2025-06-13 23:50:38 +0800

    test

---
+++ test.cpp
@@ -0,0 +1,17 @@
+int main (){
+    // int a = 10;
+    // int b = 20;
+    // int c = 30;
+    // int d = 40;
+    // int e = 50;
+    // int f = 60;
+    // int g = 70;
+    // int h = 80;
+    // int i = 90;
+    // int j = 100;
+    // int k = 110;
+    // int l = 120;
+    // int m = 130;
+    // int n = 140;
+    // int o = 150;
+}
